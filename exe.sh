d7eb011ad7a327789cd2294a50d5e993a23ee36e
yoshinari nihei <n0802jp2@gmail.com>
2022-12-13 11:46:50 +0900

    fix

--- exe.sh
+++ exe.sh
@@ -16,7 +16,7 @@ fi
 old_ipv6=$(cat ./old_ipv6)
 ipv6=$(curl ipv6.icanhazip.com)
 
-curl "http://www.mydns.jp/directip.htmlMID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
+curl -O "http://www.mydns.jp/directip.html?MID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
 
 echo $ipv4 > ./old_ipv4
 echo $ipv6 > ./old_ipv6
