a43c079168266834ab0c2098186b07df820141db
yoshinari nihei <n0802jp2@gmail.com>
2023-01-01 05:16:37 +0900

    SLACK WEBHOOK URLを.env化

--- send_slack.sh
+++ send_slack.sh
@@ -1,3 +1,4 @@
 #!bin/sh
+source ./.env
 data="{\"text\":\"$1\"}"
-curl -X POST -H 'Content-type: application/json' --data $data https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data $data $SLACK_WEBHOOK_URL
