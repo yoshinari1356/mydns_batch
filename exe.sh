7d99d100b9fbcef89c6e17500935c5886b9fe47d
nihei <n0802jp2@gmail.com>
2023-02-28 20:07:33 +0900

    cron

--- exe.sh
+++ exe.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-source ./.env
-
 # debugする時有効にする
 #set -ex
+
+cd /home/pi/project/mydns_batch/
+. ./.env
 
 if [ ! -e ./old_ipv4 ]; then
   touch ./old_ipv4
@@ -16,11 +17,11 @@ fi
 old_ipv6=$(cat ./old_ipv6)
 ipv6=$(curl ipv6.icanhazip.com)
 
-curl -o ./temp.html "http://www.mydns.jp/directip.html?MID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
+curl -o temp.html "http://www.mydns.jp/directip.html?MID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
 
-. ./send_slack.sh $ipv4
-. ./send_slack.sh $ipv6
-. ./send_slack.sh `cat temp.html`
+bash ./send_slack.sh $ipv4
+bash ./send_slack.sh $ipv6
+bash ./send_slack.sh `cat temp.html`
 
 rm temp.html
 
--- send_slack.sh
+++ send_slack.sh
@@ -1,4 +1,6 @@
-#!bin/sh
-source ./.env
+#!bin/bash
+cd /home/pi/project/mydns_batch/
+. ./.env
+
 data="{\"text\":\"$1\"}"
 curl -X POST -H 'Content-type: application/json' --data $data $SLACK_WEBHOOK_URL
