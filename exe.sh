e26d807d7ca42f7e71d13b06aba6e9d9e3cfda69
nihei <n0802jp2@gmail.com>
2023-02-28 13:57:41 +0900

    feat: 送信のレスポンスを通知する

--- exe.sh
+++ exe.sh
@@ -16,10 +16,13 @@ fi
 old_ipv6=$(cat ./old_ipv6)
 ipv6=$(curl ipv6.icanhazip.com)
 
-curl -O "http://www.mydns.jp/directip.html?MID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
+curl -o ./temp.html "http://www.mydns.jp/directip.html?MID=${MYDNS_MID}&PWD=${MYDNS_PW}&IPV4ADDR=${ipv4}&IPV6ADDR=${ipv6}"
 
 . ./send_slack.sh $ipv4
 . ./send_slack.sh $ipv6
+. ./send_slack.sh `cat temp.html`
+
+rm temp.html
 
 echo $ipv4 > ./old_ipv4
 echo $ipv6 > ./old_ipv6
